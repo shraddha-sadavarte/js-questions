f74b221619b793067223abc8242c635cb397bb78
shraddha-sadavarte <shraddhasadavarte24@gmail.com>
2023-08-19 12:59:39 -0700

    function questions

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//ARRAY QUESTIONS
+//====================
+
 //Que.1 Given a string, reverse each word in the sentence
 var str = "sare jaha se accha hindustan hamara";
 var saveStr = str.split(" ").map(function(word){
@@ -38,3 +41,81 @@ function duplicate(arr){
 }
 
 console.log(duplicate([1,2,3,4,5]));
+
+//FUNCTIONS QUESTIONS
+//==============================
+
+//Que.6 Write a js function that reverse a number
+function reverseKaro(num){
+  return Number(num.toString().split("").reverse().join(""));
+}
+
+console.log(reverseKaro(12));
+console.log(reverseKaro(123452573));
+
+//reverse a number using loop
+function reverseNum(elem){
+    var rev = 0
+    while(elem>0){
+        var reminder = elem%10;
+        rev = rev*10+reminder;
+        elem = Math.floor( elem/10 );
+    }
+    return rev;
+}
+
+console.log(reverseNum(345678));
+
+//Que.7 Write a js function that checks whether a passed string is palindrome or not
+function checkPalindrome(str){
+    var revStr = str.split("").reverse().join("");
+    if(revStr == str){
+        return str+" is palindrome";
+    }
+    else{
+        return str+" is not palindrome";
+    }
+}
+
+console.log(checkPalindrome("mom"));
+console.log(checkPalindrome("siso"));
+
+//Que.8 Write a js function that returns a passed string with letters in alphabetical order
+function returnSorted(str){
+    return str.split("").sort().join("");
+}
+
+console.log(returnSorted("shraddha"));
+
+//Que.9 Write a js function that accepts a string as a parameter
+//and converts the first letter of each word of the string in uppercase
+function capitalize(str){
+   var allwords =  str.split(" ").map(function(word){
+        return word.charAt(0).toUpperCase() + word.substring(1);
+    })
+
+     return allwords.join(" ");
+}
+
+console.log(capitalize("Shraddha is an engineering student"));
+
+//Que.10 Write a js function to get the number of occurences 
+//of each letter in specified string
+function occ(str){
+    var occurences = {};
+
+    str.split("").forEach(function(elem){
+        if(occurences.hasOwnProperty(elem) == false){
+            occurences[elem] = 1;
+        }
+        else{
+            occurences[elem]++;
+        }
+    })
+
+    return occurences;
+}
+
+console.log(occ("yashraj"));
+
+
